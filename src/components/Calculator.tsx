490d46b68e6928ddfa0355f99921798ed8bd6104
Noah Dobie <NoahRDobie@gmail.com>
2025-02-22 14:35:12 -0800

    Mobile device fixes

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -31,7 +31,7 @@ type PercentageDisplayProps = {
 
 function PercentageDisplay({ percentage, oneRepMax, unit, isDarkMode }: PercentageDisplayProps) {
     return (
-        <div className={`transition flex justify-between border rounded-md p-2 ${isDarkMode ? 'border-dark-border' : 'border-light-border'} hover:border-red-900 focus:border-red-900`} >
+        <div className={`transition flex justify-between border rounded-md p-2 text-sm sm:text-sm md:text-md lg:text-lg xl:text-xl ${isDarkMode ? 'border-dark-border' : 'border-light-border'} hover:border-red-900 focus:border-red-900`} >
             <span>{percentage}%</span>
             <span>{calculateReps(oneRepMax, percentage)} x {Math.round(oneRepMax * (percentage / 100))} {unit}</span>
         </div>
@@ -42,14 +42,14 @@ function Calculator({ weight, reps, unit, isDarkMode }: CalculatorProps) {
   const oneRepMax = calculateOneRepMax(weight, reps);
 
   return (
-    <div className={`transition-colors duration-300 p-6 rounded-lg shadow-md mt-6 ${isDarkMode ? 'bg-dark-secondary text-dark-text' : 'bg-light-secondary text-light-text'}`}>
+    <div className={`transition-colors duration-300 p-4 rounded-lg shadow-md mt-3 ${isDarkMode ? 'bg-dark-secondary text-dark-text' : 'bg-light-secondary text-light-text'}`}>
         <div className="text-center mb-4">
             <div className="text-2xl font-normal flex items-center justify-center">
             <FontAwesomeIcon icon={faCrown} className="text-2xl mr-2" style={{ color: '#FFD43B' }} />
             <h1 className="text-3xl font-bold">{oneRepMax} {unit}</h1>
             </div>
         </div>
-        <div className="grid grid-cols-2 gap-3">
+        <div className="grid grid-cols-2 gap-4">
             {percentages.map((percentage) => (
             <PercentageDisplay key={percentage} percentage={percentage} oneRepMax={oneRepMax} unit={unit} isDarkMode={isDarkMode} />
             ))}
